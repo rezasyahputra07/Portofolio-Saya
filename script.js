3966bbbd35592f59e8d0b4ff1e68d584239ea27e
rezasyahputra07 <rezasyahputra0507@gmail.com>
2026-03-31 23:03:28 +0700

    update tampilan portfolio

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ function scrolltocontact(){
     });
 }
 
-document.getElementById("contactform").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("massage").innerText = 
-    "Terimakasih Telah Menghubungi Saya!";
-    this.reset();
+document.getElementById("contactform").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const nama = document.querySelector('input[type="text"]').value;
+    const email = document.querySelector('input[type="email"]').value;
+    const pesan = document.querySelector('textarea').value;
+
+    
+    const nomor = "6288215752104"; 
+
+    const text = `Halo, saya ${nama}%0AEmail: ${email}%0APesan: ${pesan}`;
+
+    alert("Anda akan diarahkan ke WhatsApp");
+    
+    window.open(`https://wa.me/${nomor}?text=${text}`, "_blank");
 });
 
 function toggleMenu() {
@@ -23,4 +33,13 @@ window.addEventListener("scroll", () => {
             sec.classList.add("show");
         }
     });
+});
+
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("active");
+}
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+    nav.classList.toggle("scrolled", window.scrollY > 50);
 });
